e21e22db36df9f313b2a572724f01dcdcf62e6ab
sydneyhoran <sydney.horan@penn-interactive.com>
2024-09-26 09:20:20 -0400

    fix calculation with commas, division, updated at time

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ title }) => {
     const { userDetails, logout } = useAuth();
-    const version = 'v1.0.0';
+    const version = 'v1.0.1';
 
     // useEffect(() => {
     //     // Initialize Sidenav
